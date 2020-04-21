211f99ad19b7b15547cd6c0df94ff9d4f4a128a9
Anton Komissarov <anton.v.komissarov@gmail.com>
2020-04-21 19:25:19 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hanabi_agents',
-    version='0.0.0',
+    version='0.0.5',
     description='An example agent for hanabi game.',
     url='https://github.com/braintimeException/hanabi_agents',
     author='braintimeException',
