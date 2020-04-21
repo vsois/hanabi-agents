5dae4e7b6a5f4236a32dde8f890571372f6c47ca
Anton Komissarov <anton.v.komissarov@gmail.com>
2020-04-21 19:05:32 +0200

    add setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hanabi_agents',
+    version='0.0.0',
+    description='An example agent for hanabi game.',
+    url='https://github.com/braintimeException/hanabi_agents',
+    author='braintimeException',
+    packages=find_packages()
+)
